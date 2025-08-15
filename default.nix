caf38525fa04ad8b36f859c93b825446f3918e55
Trevor Bernard <trevor.bernard@gmail.com>
2025-08-15 14:26:59 -0400

    feat: improve nix package with cleanSource and metadata
    Add package metadata including description, homepage, and license.
    Use cleanSource to exclude unnecessary files from the build.

--- default.nix
+++ default.nix
@@ -12,7 +12,7 @@ in
 rustPlatform.buildRustPackage {
   pname = cargoToml.package.name;
   version = cargoToml.package.version;
-  src = ./.;
+  src = pkgs.lib.cleanSource ./.;
   cargoLock = {
     lockFile = ./Cargo.lock;
   };
@@ -20,4 +20,10 @@ rustPlatform.buildRustPackage {
     pkgs.pkg-config
   ];
   buildInputs = [ ];
+  meta = with pkgs.lib; {
+    description = "A utility program that enables clipboard copying using OSC52 escape sequences";
+    homepage = "https://github.com/trevorbernard/termcopy";
+    license = licenses.mit;
+    maintainers = with maintainers; [ ];
+  };
 }

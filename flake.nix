b1561fe8a5100f963a31efa6787dc9fd9ab7c1f2
Trevor Bernard <tbernard@aypa.com>
2026-03-18 17:59:52 -0400

    feat: add Docker image build and reformat with alejandra
    Add a dockerTools.buildLayeredImage for Linux targets and
    reformat flake.nix with alejandra style.

--- flake.nix
+++ flake.nix
@@ -3,60 +3,65 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05-small";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-    }:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-darwin"
-      ];
-      forEachSupportedSystem =
-        f:
-        nixpkgs.lib.genAttrs supportedSystems (
-          system:
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-darwin"
+    ];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (
+        system:
           f {
             pkgs = import nixpkgs {
               inherit system;
             };
           }
-        );
-    in
-    {
-      formatter = forEachSupportedSystem ({ pkgs }: pkgs.alejandra);
-
-      packages = forEachSupportedSystem (
-        { pkgs }:
-        {
-          default = pkgs.callPackage ./default.nix { };
-        }
       );
+  in {
+    formatter = forEachSupportedSystem ({pkgs}: pkgs.alejandra);
 
-      devShells = forEachSupportedSystem (
-        { pkgs }:
+    packages = forEachSupportedSystem (
+      {pkgs}:
         {
-          default = pkgs.mkShell {
-            nativeBuildInputs = [
-              pkgs.cargo
-              pkgs.cargo-audit
-              pkgs.cargo-nextest
-              pkgs.clippy
-              pkgs.coreutils # for sha256sum
-              pkgs.just
-              pkgs.pkg-config
-              pkgs.rustc
-              pkgs.rust-analyzer
-              pkgs.rustfmt
-            ];
-            buildInputs = [ ];
-            shellHook = ''
-              echo "Rust $(rustc --version)"
-              echo "Cargo $(cargo --version)"
-            '';
+          default = pkgs.callPackage ./default.nix {};
+        }
+        // pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
+          dockerImage = pkgs.dockerTools.buildLayeredImage {
+            name = "termcopy";
+            tag = "latest";
+            contents = [(pkgs.callPackage ./default.nix {})];
+            config = {
+              Entrypoint = ["/bin/termcopy"];
+            };
           };
         }
-      );
-    };
+    );
+
+    devShells = forEachSupportedSystem (
+      {pkgs}: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.cargo
+            pkgs.cargo-audit
+            pkgs.cargo-nextest
+            pkgs.clippy
+            pkgs.coreutils # for sha256sum
+            pkgs.just
+            pkgs.pkg-config
+            pkgs.rustc
+            pkgs.rust-analyzer
+            pkgs.rustfmt
+          ];
+          buildInputs = [];
+          shellHook = ''
+            echo "Rust $(rustc --version)"
+            echo "Cargo $(cargo --version)"
+          '';
+        };
+      }
+    );
+  };
 }
